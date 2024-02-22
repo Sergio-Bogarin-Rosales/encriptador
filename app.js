3f2937f9df2f8ce5afcffd1595acfa5445149268
Sergio-Bogarin-Rosales <ing.sergio.bogarinr@gmail.com>
2024-02-22 03:11:55 -0600

    se agrego la fuente base y se acomodo los marcos

---
+++ app.js
@@ -0,0 +1,11 @@
+/* 
+La letra "e" es convertida para "enter"
+La letra "i" es convertida para "imes"
+La letra "a" es convertida para "ai"
+La letra "o" es convertida para "ober"
+La letra "u" es convertida para "ufat"
+*/
+
+let codigo_incriptacion = {'a': 'ai', 'e': 'enter', 'i': 'imes', 'o': 'ober', 'u': 'ufat'};
+
+console.log(codigo_incriptacion);
